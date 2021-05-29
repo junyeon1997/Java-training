046d873f1eb5a48cf401fc810a6058f9253faa4b
junyeon1997 <junyeon2012@gmail.com>
2021-05-30 00:14:56 +0900

    Fix repeat function and add prompt

--- src/jv/calender/Calender.java
+++ src/jv/calender/Calender.java
@@ -23,15 +23,21 @@ public class Calender {
 		
 		
 		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		String prompt = "cal> ";
 		Scanner scanner = new Scanner(System.in);
 		Calender cal = new Calender();
 		
-		System.out.println("반복횟수를 입력하세요.");
-		int repeat = scanner.nextInt();
-		
-		for(int i=0; i<repeat; i++) {
+		int month = 0;
+		while(true) {
 			System.out.println("달을 입력하세요.");
-			int month = scanner.nextInt(); 
+			System.out.println(prompt);
+			month = scanner.nextInt();
+			if (month ==-1) {
+				break;
+			}
+			if (month > 12) {
+				continue;
+			}
 			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getmaxDaysOfMonth(month));
 			
 		}
