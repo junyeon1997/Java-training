eb55009563b3c3bb5b26283ada66feb9fad75ee2
junyeon1997 <junyeon2012@gmail.com>
2021-06-21 18:17:39 +0900

    Add proxy pattern

--- src/com/company/design/Main.java
+++ src/com/company/design/Main.java
@@ -1,38 +1,38 @@
 package com.company.design;
 
 import com.company.design.adapter.*;
+import com.company.design.aop.AopBrowser;
+import com.company.design.proxy.Browser;
+import com.company.design.proxy.BrowserProxy;
+import com.company.design.proxy.IBrowser;
 import com.company.design.singleton.AClazz;
 import com.company.design.singleton.BClazz;
 import com.company.design.singleton.SocketClient;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Main {
 
     public static void main(String[] args) {
-/*
-        AClazz aClazz = new AClazz();
-        BClazz bClazz = new BClazz();
 
-        SocketClient aClient = aClazz.getSocketClient();
-        SocketClient bClient = bClazz.getSocketClient();
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
 
-        System.out.println("두개의 객체가 동일한가?");
-        System.out.println(aClient.equals(bClient));
- */
-        HairDryer hairDryer = new HairDryer();
-        connect(hairDryer);
+        IBrowser aopBrowser = new AopBrowser("www.naver.com",
+                ()->{
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+                },
+                ()->{
+                    long now = System.currentTimeMillis();
+                    end.set(now - start.get());
+                }
+                );
+        aopBrowser.show();
+        System.out.println("loading time : " + end.get());
 
-        Cleaner cleaner = new Cleaner();
-        //connect(cleaner); 220v 어뎁터 필요
-        Electronic110V adapter = new SocketAdapter(cleaner);
-        connect(adapter);
-
-        AirConditioner airConditioner = new AirConditioner();
-        Electronic110V adapter1 = new SocketAdapter(airConditioner);
-        connect(adapter1);
+        aopBrowser.show();
+        System.out.println("loading time : " + end.get());
     }
 
-    //콘센트
-    public static void connect(Electronic110V electronic110V) {
-        electronic110V.powerOn();
-    }
 }
