ecbd239ee9907325f5c50794b029db44eb0732e7
junyeon1997 <junyeon2012@gmail.com>
2021-06-22 00:02:51 +0900

    Add: strategy pattern 문자열 encoding 방식을 세가지로 구현

--- src/com/company/design/Main.java
+++ src/com/company/design/Main.java
@@ -1,22 +1,31 @@
 package com.company.design;
 
-import com.company.design.facade.Ftp;
-import com.company.design.facade.Reader;
-import com.company.design.facade.SftpClient;
-import com.company.design.facade.Writer;
+import com.company.design.strategy.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        SftpClient sftpClient = new SftpClient("www.example.co.kr",22,"/home/etc","text.tmp");
 
-        sftpClient.connect();
+        Encoder encoder = new Encoder();
 
-        sftpClient.write();
+        //base64
+        EncodingStrategy base64 = new Base64Strategy();
+        //normal
+        EncodingStrategy normal = new NormalStrategy();
 
-        sftpClient.read();
+        String message = "hello java";
 
-        sftpClient.disConnect();
+        encoder.setEncodingStrategy(base64);
+        String base64Result = encoder.getMessage(message);
+        System.out.println(base64Result);
+
+        encoder.setEncodingStrategy(normal);
+        String normalResult = encoder.getMessage(message);
+        System.out.println(normalResult);
+
+        encoder.setEncodingStrategy(new AppendStrategy());
+        String appendResult = encoder.getMessage(message);
+        System.out.println(appendResult);
     }
 
 }
