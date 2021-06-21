13813e35f8737e674b368060d8d1c66c31ba6acf
junyeon1997 <junyeon2012@gmail.com>
2021-06-21 16:38:59 +0900

    Add adapter pattern

--- src/com/company/design/Main.java
+++ src/com/company/design/Main.java
@@ -1,5 +1,6 @@
 package com.company.design;
 
+import com.company.design.adapter.*;
 import com.company.design.singleton.AClazz;
 import com.company.design.singleton.BClazz;
 import com.company.design.singleton.SocketClient;
@@ -7,7 +8,7 @@ import com.company.design.singleton.SocketClient;
 public class Main {
 
     public static void main(String[] args) {
-
+/*
         AClazz aClazz = new AClazz();
         BClazz bClazz = new BClazz();
 
@@ -16,5 +17,22 @@ public class Main {
 
         System.out.println("두개의 객체가 동일한가?");
         System.out.println(aClient.equals(bClient));
+ */
+        HairDryer hairDryer = new HairDryer();
+        connect(hairDryer);
+
+        Cleaner cleaner = new Cleaner();
+        //connect(cleaner); 220v 어뎁터 필요
+        Electronic110V adapter = new SocketAdapter(cleaner);
+        connect(adapter);
+
+        AirConditioner airConditioner = new AirConditioner();
+        Electronic110V adapter1 = new SocketAdapter(airConditioner);
+        connect(adapter1);
+    }
+
+    //콘센트
+    public static void connect(Electronic110V electronic110V) {
+        electronic110V.powerOn();
     }
 }
